e54eed2975a90c3a9615ebcf54eb09ab7d7298b8
Safal017 <60866001+Safal017@users.noreply.github.com>
2020-02-10 18:16:41 +0545

    Cru features with testing screenshots

--- django_coursework/app/views.py
+++ django_coursework/app/views.py
@@ -1,3 +1,295 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import *
+from django.http import HttpResponse
+from .models import *
+
+def view_home_page(request):
+        return render(request,'index.html')
+        
+def view_students_lists(request):
+    list_of_students= Student.objects.all()
+
+    context_variable = {
+        'student':list_of_students
+    }
+    return render(request,'students/students_list.html',context_variable)
+
+def view_students_list_page(request):
+    return render(request,'students/students_list.html')
+
+def view_student_form(request):
+    return render(request,'students/student_form.html')
+
+def view_studentdata_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_first_name = request.POST['first_name']
+        get_last_name = request.POST['last_name']
+        get_phone_no = request.POST['phone_no']
+        get_address = request.POST['address']
+        students_obj = Student(first_name=get_first_name,last_name=get_last_name,phone_no=get_phone_no,address=get_address)
+        students_obj.save()
+        return redirect('/app/studentlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_Studentdata_updateform(request,ID):
+    students_obj = Student.objects.get(id=ID)
+    context_varible = {
+        'students': students_obj
+    }
+    return render(request,'students/update_student.html',context_varible)
+
+def view_student_update_form_data_in_db(request,ID):
+    students_obj = Student.objects.get(id=ID)
+    student_form_data = request.POST
+    students_obj.first_name = request.POST['first_name']
+    students_obj.last_name = request.POST['last_name']
+    students_obj.phone_no = request.POST['phone_no']
+    students_obj.address = request.POST['address']
+    students_obj.save()
+    return redirect('/app/studentlist/')
+
+
+
+def view_teacher_lists(request):
+    list_of_teachers= Teacher.objects.all()
+
+    context_variable = {
+        'teacher':list_of_teachers
+    }
+    return render(request,'teacher/teacher_list.html',context_variable)
+
+def view_teachers_list_page(request):
+    return render(request,'teacher/teacher_list.html')
+
+def view_teacher_form(request):
+    return render(request,'teacher/teacher_form.html')
+
+def view_teacherdata_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_first_name = request.POST['first_name']
+        get_last_name = request.POST['last_name']
+        get_email = request.POST['email']
+        get_address = request.POST['address']
+        get_subject= request.POST['subject']
+        teacher_obj = Teacher(first_name=get_first_name,last_name=get_last_name,email=get_email,address=get_address,subject=get_subject)
+        teacher_obj.save()
+        return redirect('/app/teacherlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_teacherdata_updateform(request,ID):
+    teacher_obj = Teacher.objects.get(id=ID)
+    context_varible = {
+        'teachers': teacher_obj
+    }
+    return render(request,'teacher/update_teacher.html',context_varible)
+
+def view_teacher_update_form_data_in_db(request,ID):
+    teacher_obj = Teacher.objects.get(id=ID)
+    teacher_form_data = request.POST
+    teacher_obj.first_name = request.POST['first_name']
+    teacher_obj.last_name = request.POST['last_name']
+    teacher_obj.email = request.POST['email']
+    teacher_obj.address = request.POST['address']
+    teacher_obj.subject = request.POST['subject']
+    teacher_obj.save()
+    return redirect('/app/teacherlist/')
+
+
+def view_subject_lists(request):
+    list_of_subject= Subject.objects.all()
+
+    context_variable = {
+        'subject':list_of_subject
+    }
+
+    return render(request,'subject/subject_list.html',context_variable)
+
+
+def view_subject_list_page(request):
+    return render(request,'subject/subject_list.html')
+
+def view_subject_form(request):
+    return render(request,'subject/subject_form.html')
+
+def view_subjectdata_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_code = request.POST['code']
+        get_name = request.POST['name']
+        get_class_name = request.POST['class_name']
+        subject= Subject(code=get_code,name=get_name,class_name=get_class_name)
+        subject.save()
+        return redirect('/app/subjectlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_subjectdata_updateform(request,ID):
+    subject = Subject.objects.get(id=ID)
+    context_varible = {
+        'subjects': subject
+    }
+    return render(request,'subject/update_subject.html',context_varible)
+
+def view_subject_update_form_data_in_db(request,ID):
+    subject = Subject.objects.get(id=ID)
+    list_of_subject = request.POST
+    subject.code = request.POST['code']
+    subject.name= request.POST['name']
+    subject.class_name = request.POST['class_name']
+    subject.save()
+    return redirect('/app/subjectlist/')
+
+
+def view_class_lists(request):
+    list_of_class= Class.objects.all()
+
+    context_variable = {
+        'class':list_of_class
+    }
+
+    return render(request,'class/class_list.html',context_variable)
+
+
+def view_class_list_page(request):
+    return render(request,'class/class_list.htmll')
+
+def view_class_form(request):
+    return render(request,'class/class_form.html')
+
+def view_class_data_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_code = request.POST['code']
+        get_class_name = request.POST['class_name']
+        get_class_type = request.POST['class_type']
+        class_obj= Class(code=get_code,class_name=get_class_name,class_type=get_class_type)
+        class_obj.save()
+        return redirect('/app/classlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_class_updateform(request,ID):
+    class_obj = Class.objects.get(id=ID)
+    context_varible = {
+        'classes': class_obj
+    }
+    return render(request,'class/update_class.html',context_varible)
+
+def view_class_update_form_data_in_db(request,ID):
+    class_obj = Class.objects.get(id=ID)
+    list_of_class= request.POST
+    class_obj.code = request.POST['code']
+    class_obj.class_name= request.POST['class_name']
+    class_obj.class_type = request.POST['class_type']
+    class_obj.save()
+    return redirect('/app/classlist/')
+
+def view_parent_lists(request):
+    list_of_parent= Parents.objects.all()
+
+    context_variable = {
+        'parent': list_of_parent    
+    }
+
+    return render(request,'parent/parent_list.html',context_variable)
+
+
+def view_parent_list_page(request):
+    return render(request,'parent/parent_list.html')
+
+def view_parent_form(request):
+    return render(request,'parent/parent_form.html')
+
+def view_parent_data_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_first_name = request.POST['first_name']
+        get_last_name = request.POST['last_name']
+        get_phone_no = request.POST['phone_no']
+        get_address = request.POST['address']
+        parent_obj = Parents(first_name=get_first_name,last_name=get_last_name,phone_no=get_phone_no,address=get_address)
+        parent_obj.save()
+        return redirect('/app/parentlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_parent_updateform(request,ID):
+    parent_obj = Parents.objects.get(id=ID)
+    context_varible = {
+        'parents': parent_obj
+    }
+    return render(request,'parent/update_parent.html',context_varible)
+
+def view_parent_update_form_data_in_db(request,ID):
+    parent_obj = Parents.objects.get(id=ID)
+    parent_form_data = request.POST
+    parent_obj.first_name = request.POST['first_name']
+    parent_obj.last_name = request.POST['last_name']
+    parent_obj.phone_no = request.POST['phone_no']
+    parent_obj.address = request.POST['address']
+    parent_obj.save()
+    return redirect('/app/parentlist/')
+
+
+def view_result_lists(request):
+    list_of_result= Result.objects.all()
+
+    context_variable = {
+        'result': list_of_result   
+    }
+
+    return render(request,'result/result_list.html',context_variable)
+
+
+def view_result_list_page(request):
+    return render(request,'result/result_list.html')
+
+def view_result_form(request):
+    return render(request,'result/result_form.html')
+
+def view_result_data_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_student_id = request.POST['student_id']
+        get_student_name = request.POST['student_name']
+        get_subject_code = request.POST['subject_code']
+        get_subject_name = request.POST['subject_name']
+        get_teacher_name = request.POST['teacher_name']
+        get_marks_obtained = request.POST['marks_obtained']
+        result_obj = Result(student_id=get_student_id,student_name=get_student_name,subject_code=get_subject_code,subject_name=get_subject_name,teacher_name=get_teacher_name,marks_obtained=get_marks_obtained)
+        result_obj.save()
+        return redirect('/app/resultlist/')
+    else:
+        return HttpResponse("Error record saving")
+
+
+def view_result_updateform(request,ID):
+    result_obj = Result.objects.get(id=ID)
+    context_varible = {
+        'results': result_obj
+    }
+    return render(request,'result/update_result.html',context_varible)
+
+def view_result_update_form_data_in_db(request,ID):
+    result_obj = Result.objects.get(id=ID)
+    result_form_data = request.POST
+    result_obj.student_id = request.POST['student_id']
+    result_obj.student_name = request.POST['student_name']
+    result_obj.subject_code = request.POST['subject_code']
+    result_obj.subject_name = request.POST['subject_name']
+    result_obj.teacher_name = request.POST['teacher_name']
+    result_obj.marks_obtained = request.POST['marks_obtained']
+
+    result_obj.save()
+    return redirect('/app/resultlist/')
